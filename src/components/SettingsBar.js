4a942983ea444eaab620859d47526ff0589ab028
Coding Chancez <codingchancez@gmail.com>
2019-10-27 09:08:53 +0800

    Added settings bar

---
+++ src/components/SettingsBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
+import ThemeToggle from './ThemeToggle';
+
+const useStyles = makeStyles(theme => ({
+  settings: {
+    paddingRight: theme.spacing(4),
+  },
+}));
+
+export default () => {
+  const classes = useStyles();
+  return (
+    <Grid
+      container
+      className={classes.settings}
+      alignItems="center"
+      justify="flex-end"
+    >
+      <ThemeToggle />
+    </Grid>
+  );
+}
